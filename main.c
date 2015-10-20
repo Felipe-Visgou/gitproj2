3f10c9d813d6effd9571b6fece5e5c5ddf1dca22
Felipe VieiraCortes <felipevieiracortes@gmail.com>
2015-10-19 22:41:43 -0200

    main em andamento

---
+++ main.c
@@ -0,0 +1,127 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BAR.h"
+#include "DADO.H"
+#include "DADOPONTOS.h"
+#include "LISTA.H"
+#include "PecasFinalizadas.h"
+#include "TABULEIRO.H"
+#include "PECA.h"
+// funcao destruir valor
+
+   static void DestruirValor( void * pValor ) ;
+// Imprimir o estado do jogo
+
+	void  displayJogo(LIS_tppLista estrutura);
+
+int main (void)
+{
+	// variaveis
+	LIS_tppLista Principal = LIS_CriarLista(DestruirValor);
+	TAB_tppTabuleiro tabuleiro; // tabuleiro
+	PF_tppFinalizadas pfbranca, pfpreta; // pecas finalizadas
+	BAR_tppCapturadas barbranca, barpreta; // pecas capturadas
+	int dado1, dado2; // dados da partida
+	tppDadoPontos dp;
+	char preto = 'p', branco = 'b';
+	int tampfb, tampfp; // tamanho da estrutura de pecas finalizadas, elas que determinam o termino do jogo
+
+	// Criar criar as estruturas e adicionalas na principal
+
+	TAB_CriarTabuleiro(&tabuleiro, DestruirValor);
+	PF_CriarPF(&pfbranca, preto, DestruirValor);
+	PF_CriarPF(&pfpreta, preto, DestruirValor);
+	BAR_CriarBAR(&barbranca, branco, DestruirValor);
+	BAR_CriarBAR(&barpreta, preto, DestruirValor);
+	DADPnt_CriarDado(&dp);
+
+	LIS_InserirElementoApos(Principal, tabuleiro);
+	LIS_InserirElementoApos(Principal, barbranca);
+	LIS_InserirElementoApos(Principal, pfbranca);
+	LIS_InserirElementoApos(Principal, barpreta);
+	LIS_InserirElementoApos(Principal, pfpreta);
+	LIS_InserirElementoApos(Principal, dp);
+
+	PF_ObterTamanho(pfbranca, &tampfb);
+	PF_ObterTamanho(pfpreta, &tampfp);
+
+	while(tampfb < 12 || tampfp < 12)
+	{
+
+		// jogar
+	}
+	//fim de jogo
+}
+void  displayJogo(LIS_tppLista estrutura)
+{
+	// variaveis
+	TAB_tppTabuleiro tab; // tabuleiro
+	PF_tppFinalizadas pfb, pfp; // pecas finalizadas
+	BAR_tppCapturadas barb, barp; // pecas capturadas
+	tppDadoPontos dp;
+	char preto = 'p', branco = 'b', corpeca;
+	int tampfb, tampfp; // tamanho da estrutura de pecas finalizadas, elas que determinam o termino do jogo
+	LIS_tppLista casas;
+	tppPeca peca;
+
+	// obter o valor de cada estrutura
+	IrInicioLista(estrutura);
+	tab = (TAB_tppTabuleiro)LIS_ObterValor(estrutura);
+	int i, j;
+	char valor;
+	if(LIS_AvancarElementoCorrente(estrutura, 1) != LIS_CondRetOK)
+	{
+		printf("Erro ao andar na estrutura principal (display) \n");
+		exit(-1);
+	}
+
+	barb = (BAR_tppCapturadas)LIS_ObterValor(estrutura);
+	if(LIS_AvancarElementoCorrente(estrutura, 1) != LIS_CondRetOK)
+	{
+		printf("Erro ao andar na estrutura principal (display) \n");
+		exit(-1);
+	}
+	pfb = (PF_tppFinalizadas)LIS_ObterValor(estrutura);
+	if(LIS_AvancarElementoCorrente(estrutura, 1) != LIS_CondRetOK)
+	{
+		printf("Erro ao andar na estrutura principal (display) \n");
+		exit(-1);
+	}
+
+	barp = (BAR_tppCapturadas)LIS_ObterValor(estrutura);
+	if(LIS_AvancarElementoCorrente(estrutura, 1) != LIS_CondRetOK)
+	{
+		printf("Erro ao andar na estrutura principal (display) \n");
+		exit(-1);
+	}
+	pfp = (PF_tppFinalizadas)LIS_ObterValor(estrutura);
+	if(LIS_AvancarElementoCorrente(estrutura, 1) != LIS_CondRetOK)
+	{
+		printf("Erro ao andar na estrutura principal (display) \n");
+		exit(-1);
+	}
+	dp = (tppDadoPontos)LIS_ObterValor(estrutura);
+
+	// parte dificil (imprimir)
+
+	// imrpimir 12 casas
+
+	printf("| 13 - 14 - 15 - 16 - 17 - 18 - 19 - 20 - 21 - 22 - 23 - 24 | \n  ");
+	for(i = 0; i < 12; i++)
+	{
+		for(j = 0; j < 12; j++)
+		{
+			if(TAB_ObterCasas(tab, &casas) != TAB_CondRetOK)
+			{
+				printf("Erro ao obter as casas (display) \n");
+				exit(-1);
+			}
+			peca = (tppPeca)LIS_ObterValor(casas); // errado
+			Pec_ObterCor(peca, &corpeca);
+			if(corpeca == preto)
+				valor = 178;
+			else
+				valor = 176;
+			printf("%c    ", valor);
+			LIS_AvancarElementoCorrente( // casa de casas é o tabulerio tem q mudar esta porra
+			
